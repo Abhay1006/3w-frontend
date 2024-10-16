213c65cf93545e8680bf66b9a96eb4b4768bf06a
Abhay1006 <abhaypratapsingh1006@gmail.com>
2024-10-16 20:17:14 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { io } from 'socket.io-client';
-import './App.css'; // Import the CSS file for styling
+import './App.css'; 
 
 const socket = io('http://localhost:5000');
 
@@ -12,18 +12,15 @@ function App() {
   const [pointsAwarded, setPointsAwarded] = useState(null);
   const [newUserName, setNewUserName] = useState('');
 
-  // Fetch initial users
   useEffect(() => {
     axios.get('http://localhost:5000/users').then(res => {
       setUsers(res.data);
     });
 
-    // Listen for leaderboard updates
     socket.on('leaderboard-update', (updatedUsers) => {
       setLeaderboard(updatedUsers);
     });
 
-    // Fetch initial leaderboard
     axios.get('http://localhost:5000/leaderboard').then(res => {
       setLeaderboard(res.data);
     });
@@ -42,8 +39,8 @@ function App() {
     if (newUserName) {
       axios.post('http://localhost:5000/users', { name: newUserName })
         .then(res => {
-          setUsers([...users, res.data]); // Add the new user to the list
-          setNewUserName(''); // Clear input field
+          setUsers([...users, res.data]); 
+          setNewUserName(''); 
         })
         .catch(err => console.error(err));
     }
@@ -53,7 +50,6 @@ function App() {
     <div className="App">
       <h1 className="title">Leaderboard</h1>
 
-      {/* User Selection */}
       <div className="user-selection">
         <select className="user-dropdown" onChange={e => setSelectedUser(e.target.value)} value={selectedUser}>
           <option value="">Select a User</option>
@@ -64,10 +60,8 @@ function App() {
         <button className="claim-btn" onClick={claimPoints} disabled={!selectedUser}>Claim Points</button>
       </div>
 
-      {/* Display Awarded Points */}
       {pointsAwarded && <p className="points-awarded">{pointsAwarded}</p>}
 
-      {/* Add New User */}
       <h2 className="subtitle">Add New User</h2>
       <form className="add-user-form" onSubmit={addUser}>
         <input
@@ -81,7 +75,6 @@ function App() {
         <button className="add-btn" type="submit">Add User</button>
       </form>
 
-      {/* Leaderboard */}
       <h2 className="subtitle">Current Leaderboard</h2>
       <ul className="leaderboard">
         {leaderboard.map((user, index) => (
